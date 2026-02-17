8511ff073549e2178bc31c6439155cf8852f35ce
Arturo Alvarado <arturo.alvaradovic@gmail.com>
2026-02-17 14:05:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5934658721-f4806",
+  "appId": "1:190554663956:web:3bd352ee6e8cff764acb01",
+  "apiKey": "AIzaSyB7IKuoLEyBNL1mOzUiFRVKqxA6RkWReAY",
+  "authDomain": "studio-5934658721-f4806.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "190554663956"
+};
